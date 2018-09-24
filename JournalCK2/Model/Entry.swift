d77d49e1734ebbe886b481d0285f4ca5672a2bbd
Kamil Wrobel <wrobelk@live.com>
2018-09-24 15:19:24 -0600

    creates Entry Controller

--- JournalCK2/Model/Entry.swift
+++ JournalCK2/Model/Entry.swift
@@ -7,31 +7,53 @@
 //
 
 import Foundation
-
+import CloudKit
 class Entry {
     
+    //MARK: - CloudKit Keys
+    //#2 create keys
+    static let TypeKey = "Entry"
+    static let titleKey = "Title"
+    static let notesKey = "Notes"
+    static let dateKey = "Date"
     
+    
+    //MARK: - Properties
     let title: String
     let notes: String
     let date: Date
     
+    
+    //MARK: - Designated Initializer
     init(title: String, notes: String, date: Date = Date()){
         self.title = title
         self.notes = notes
         self.date = date
     }
-//    
-//    //#1 keys
-//    //#2 record
-//    //#3convert to record
-//    
-//    //#4failable init
-//    
-//    var ckRecord = 
     
     
+    //MARK: - Cloud Kit Record
+    //#1 create CKRecord computed property
+    var cloudKitRecord : CKRecord = {
+        //create record
+        let record = CKRecord(recordType: Entry.TypeKey)
+        //#3 convert properties to record
+        record.setValue("title", forKey: Entry.titleKey)
+        record.setValue("notes", forKey: Entry.notesKey)
+        record.setValue("date", forKey: Entry.dateKey)
+        
+        return record
+    }()
     
     
-    
-    
+    //MARK: - Failable Initializer
+    //#4 failble init
+    convenience init?(cloudKitRecord: CKRecord) {
+        //#5 unpack record, making usre all properties are correct type
+        guard let title = cloudKitRecord[Entry.titleKey] as? String,
+            let notes = cloudKitRecord[Entry.notesKey] as? String,
+            let date = cloudKitRecord[Entry.dateKey] as? Date else {return nil}
+        
+        self.init(title: title, notes: notes, date: date)
+    }
 }
